a2c40672ee5f2a335ace4a39d648ce2c35ad3fc9
Robert Greathouse <greathouse.robert@gmail.com>
2012-12-10 21:18:52 -0600

    Add error handling

--- Porpoise.groovy
+++ Porpoise.groovy
@@ -33,6 +33,7 @@ def scriptDirectoryPath = opts.d ?: System.getProperty('user.dir')
 scriptDirectory = new File(scriptDirectoryPath)
 
 sql = Sql.newInstance(dbUrl, dbUser, dbPassword, 'net.sourceforge.jtds.jdbc.Driver')
+
 databaseProduct = sql.connection.metaData.databaseProductName
 println "Connected to a \"${databaseProduct}\" database"
 
@@ -42,24 +43,28 @@ checkAndCreateLogTable()
 
 def scripts = determineScriptsToRun()
 if (!dryRun) {
-	scripts.each { scriptMetadata ->
-		executeScript(scriptMetadata)
+	try {
+		scripts.each { scriptMetadata ->
+			executeScript(scriptMetadata)
+		}
+	}
+	catch (AbortException) {
+		//Stop executing scripts
 	}
 }
 
 println '\n---------------------------------------'
-def ups = scripts.findAll{it.needsUp}
+def ups = scripts.findAll{it.needsUp && it.applied}
 if (ups) {
-	println 'Appling the following...'
+	println '\nApplied the following:'
 	ups.each {
 		println "\t${it.changeset}/${it.script}"
 	}
 }
 
-def downs = scripts.findAll{it.needsDown}
+def downs = scripts.findAll{it.needsDown && it.applied}
 if (downs) {
-	if (forceRemovals) { println 'Removed the following:' }
-	else { println 'Scripts marked for removal, use "-F" option to force removals'}
+	println '\nRemoved the following:'
 	downs.each {
 		println "\t${it.changeset}/${it.script}"
 	}
@@ -67,12 +72,37 @@ if (downs) {
 
 def changed = scripts.findAll{it.hasChanged}
 if (changed) {
-	println 'WARNING: THE FOLLOWING HAVE CHANGED'
+	println '\nWARNING: THE FOLLOWING HAVE CHANGED'
 	changed.each {
 		println "\t${it.changeset}/${it.script}"
 	}
 }
 
+def failed = scripts.findAll{it.failed}
+if (failed) {
+	println '\nFailed on the following scripts. (NOTE: These scripts may be paritally applied)'
+	failed.each {
+		println "\t${it.changeset}/${it.script}"
+		println "\t\tFailed Statement: ${it.failedStatement}"
+		println "\t\t${it.failedException.message}"
+	}
+}
+
+def needingUp = scripts.findAll { it.needsUp && !it.applied && !it.failed }
+if (needingUp) {
+	println '\nThe following need applied, but were not applied (probably due to a prior failure)'
+	needingUp.each {
+		println "\t${it.changeset}/${it.script}"
+	}
+}
+
+def needingDown = scripts.findAll { it.needsDown && !it.applied }
+if (needingDown) {
+	println '\nThe following need to be removed, use "-F" option to force removals'
+	needingDown.each {
+		println "\t${it.changeset}/${it.script}"
+	}
+}
 
 println 'Done!'
 
@@ -158,20 +188,30 @@ def determineScriptsToRun() {
 }
 
 def executeScript(scriptMetadata) {
-	if (scriptMetadata.needsUp) {
-		scriptMetadata.up.split(";").each {
-			executeSql(it)
+	try {
+		if (scriptMetadata.needsUp) {
+			scriptMetadata.up.split(";").each {
+				executeSql(it)
+			}
+			executeSql("insert into porp_schema_log (id, changeset, script_name, md5, date_applied, up_script, down_script) values (${UUID.randomUUID().toString()}, ${scriptMetadata.changeset}, ${scriptMetadata.script}, ${scriptMetadata.md5}, ${new java.sql.Timestamp(new Date().time)}, ${scriptMetadata.up}, ${scriptMetadata.down});")
+			scriptMetadata.applied = true
+			return
 		}
-		executeSql("insert into porp_schema_log (id, changeset, script_name, md5, date_applied, up_script, down_script) values (${UUID.randomUUID().toString()}, ${scriptMetadata.changeset}, ${scriptMetadata.script}, ${scriptMetadata.md5}, ${new java.sql.Timestamp(new Date().time)}, ${scriptMetadata.up}, ${scriptMetadata.down});")
-		return
+		
+		if (forceRemovals && scriptMetadata.needsDown) {
+			scriptMetadata.down.split(";").each {
+				executeSql(it)
+			}
+			executeSql("delete from porp_schema_log where md5 = ${scriptMetadata.md5}")
+			scriptMetadata.applied = true
+			return
+		}
 	}
-	
-	if (forceRemovals && scriptMetadata.needsDown) {
-		scriptMetadata.down.split(";").each {
-			executeSql(it)
-		}
-		executeSql("delete from porp_schema_log where md5 = ${scriptMetadata.md5}")
-		return
+	catch (SqlStatementExecutionException all) {
+		scriptMetadata.failed = true
+		scriptMetadata.failedStatement = all.stmt
+		scriptMetadata.failedException = all
+		throw new AbortException(all)
 	}
 }
 
@@ -180,7 +220,14 @@ def executeSql(def stmt) {
 	
 	if (dryRun) return
 	
-	sql.execute(stmt)
+	try {
+		sql.execute(stmt)
+	}
+	catch (java.sql.SQLException all) {
+		def e = new SqlStatementExecutionException(all)
+		e.stmt = stmt
+		throw e
+	}
 }
 
 def generateMd5(final file) {
@@ -195,4 +242,13 @@ def generateMd5(final file) {
    byte[] md5sum = digest.digest()
    BigInteger bigInt = new BigInteger(1, md5sum)
    return bigInt.toString(16)
+}
+
+private class AbortException extends Exception {
+	public AbortException(Throwable e) { super(e) }
+}
+
+private class SqlStatementExecutionException extends Exception {
+	public String stmt
+	public SqlStatementExecutionException(Throwable e) { super(e) }
 }
